94e9b75bc55ee19672c5bba9c9834073b77338a5
たつぞう <kendellcarol@gmail.com>
2023-02-09 13:22:58 +0900

    Initial commit

---
+++ Demo_macOS_ListSelections/Demo_macOS_ListSelectionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_macOS_ListSelectionsApp.swift
+//  Demo_macOS_ListSelections
+//
+//  Created by 荒木辰造 on R 5/02/09.
+//
+
+import SwiftUI
+
+@main
+struct Demo_macOS_ListSelectionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
